885ac5b2e2b4b1c3604a47507e419498e02dca32
AkramBadah <akram.badah@gmail.com>
2022-08-11 16:09:25 +0300

    Implemented products section in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
-import type { NextPage } from 'next'
-import FeaturedCategoriesSection from '../components/FeaturedCategoriesSection'
-import HeroSection from '../components/HeroSection'
-import MainLayout from '../layouts/MainLayout'
-import Box from '@mui/material/Box'
+import type { NextPage } from 'next';
+import FeaturedCategoriesSection from '../components/FeaturedCategoriesSection';
+import HeroSection from '../components/HeroSection';
+import MainLayout from '../layouts/MainLayout';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
+import VerticalProductCard from '../components/VerticalProductCard';
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +15,58 @@ const Home: NextPage = () => {
         <HeroSection />
         <Box pt={22.25} pb={11.25}>
           <FeaturedCategoriesSection />
+        </Box>
+        <Box component='section' >
+          <Container maxWidth={false} sx={{ px: {xs: 2, lg: 7.5} }}>
+            <Divider />
+            <Grid container spacing={3.75} rowSpacing={1.25} sx={{ pt: 5.25, pb: 18.25 }}>
+              <Grid item xs={2.4}>
+                <VerticalProductCard />
+              </Grid>
+              <Grid item xs={2.4}>
+                <VerticalProductCard />
+              </Grid>
+              <Grid item xs={2.4}>
+                <VerticalProductCard />
+              </Grid>
+              <Grid item xs={2.4}>
+                <VerticalProductCard />
+              </Grid>
+              <Grid item xs={2.4}>
+                <VerticalProductCard />
+              </Grid>
+              <Grid item xs={2.4}>
+                <VerticalProductCard />
+              </Grid>
+              <Grid item xs={2.4}>
+                <VerticalProductCard />
+              </Grid>
+              <Grid item xs={2.4}>
+                <VerticalProductCard />
+              </Grid>
+              <Grid item xs={2.4}>
+                <VerticalProductCard />
+              </Grid>
+              <Grid item xs={2.4}>
+                <VerticalProductCard />
+              </Grid>
+              <Grid item xs={2.4}>
+                <VerticalProductCard />
+              </Grid>
+              <Grid item xs={2.4}>
+                <VerticalProductCard />
+              </Grid>
+              <Grid item xs={2.4}>
+                <VerticalProductCard />
+              </Grid>
+              <Grid item xs={2.4}>
+                <VerticalProductCard />
+              </Grid>
+              <Grid item xs={2.4}>
+                <VerticalProductCard />
+              </Grid>
+            </Grid>
+          </Container>
         </Box>
       </>
     </MainLayout>
