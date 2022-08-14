be4552eee75bbb0aa50c0b8e288ff6ce9ed439f5
Nermen yehia <n.yehia@glt.co>
2022-08-14 14:49:14 +0200

    handle active list item

--- components/SideMenuItem/components/ListMenuItem.tsx
+++ components/SideMenuItem/components/ListMenuItem.tsx
@@ -1,14 +1,13 @@
-import Box from "@mui/material/Box";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import Image from "next/image";
-import ArrowRight from "../../../assets/images/icons/arrow-right.png";
-import { FC } from "react";
-import React from "react";
-import Menu from "@mui/material/Menu";
-import Link from "next/link";
-
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Image from 'next/image';
+import ArrowRight from '../../../assets/images/icons/arrow-right.png';
+import { FC, useState } from 'react';
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 interface Props {
   data: {
@@ -18,23 +17,25 @@ interface Props {
   };
 }
 const ListMenuItem: FC<Props> = ({ data }) => {
+  const router = useRouter();
   return (
     <ListItem
       disablePadding
       sx={{
-        borderColor: "primary.main",
-        width: "70%",
-        borderRadius: "8px",
+        backgroundColor: router.pathname === data.link ? 'grey.1600' : '',
+        borderColor: 'primary.main',
+        width: '350px',
+        height: '67px',
+        borderRadius: '8px',
       }}
       key={data.id}
     >
+        <Link href={data.link}>
       <ListItemButton disableRipple>
-        <ListItemText primary={data.name} sx={{ opacity: 1 }} />        
-                  <Link  href={data.link}>
-                   <Image src={ArrowRight} alt="Arrow right" />
-                  </Link>
-             
+        <ListItemText primary={data.name} sx={{ opacity: 1 }} />
+          <Image src={ArrowRight} alt='Arrow right' />
       </ListItemButton>
+      </Link>
     </ListItem>
   );
 };
