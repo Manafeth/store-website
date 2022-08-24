c319ed1fa8338dedd63a7dd240322dc4694c7cd2
Nermen yehia <n.yehia@glt.co>
2022-08-24 12:39:19 +0200

    handle profile setting address managment integration api

--- pages/addressManagment/index.tsx
+++ pages/addressManagment/index.tsx
@@ -1,69 +1,72 @@
-
-import React, { FormEvent, useEffect, useState } from "react";
-import AddressManagment from "../../components/AddressManagment";
-import { useProfileModal } from "../../contexts/ProfileModalContext";
-import MainLayout from "../../layouts/MainLayout";
-import ProfileLayout from "../../layouts/ProfileLayout";
+import React, { FormEvent, useEffect, useState } from 'react';
+import AddressManagment from '../../components/AddressManagment';
+import { useProfileModal } from '../../contexts/ProfileModalContext';
+import MainLayout from '../../layouts/MainLayout';
+import ProfileLayout from '../../layouts/ProfileLayout';
 import Box from '@mui/material/Box';
-import { addressDetailsData } from "../../types/profile";
-
+import { addressDetailsData } from '../../types/profile';
 
 const Setting = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const {triggerCreateAddress} = useProfileModal();
-  const [accountAddressData, setAccountAddressData] = useState<addressDetailsData>({
-    id: 0,
-    cityId: 0,
-    address: '',
-    street: '',
-    type: 0,
-    latitude:0,
-    longitude:0,
-})
-function isFormValid() {
-  return (
-    accountAddressData.address
-    && accountAddressData.street
-    && accountAddressData.cityId
-  );
-}
- 
+  const { triggerCreateAddress } = useProfileModal();
+  const [accountAddressData, setAccountAddressData] =
+    useState<addressDetailsData>({
+      id: 0,
+      cityId: 0,
+      address: '',
+      street: '',
+      type: 0,
+      latitude: 0,
+      longitude: 0,
+    });
+  function isFormValid() {
+    return (
+      accountAddressData.address &&
+      accountAddressData.street &&
+      accountAddressData.cityId
+    );
+  }
+
   function handleSubmit(ev: FormEvent<HTMLFormElement>) {
     ev.preventDefault();
-   console.log('clicked')
-//    if (!isFormValid()) {
-//     updateAddressData(accountAddressData).then(() => {
-//         setIsSubmitted(false);
-//         setAccountAddressData((prevState) => ({
-//             ...prevState,
-//         }));
-//     }).catch(() => {
-//         setIsSubmitted(false);
-//     })
-// } else {
-//     setIsSubmitted(true)
-// }
+    console.log('clicked');
+        // updateAddressData(accountAddressData).then(() => {
+        //     setIsSubmitted(false);
+        //     setAccountAddressData((prevState) => ({
+        //         ...prevState,
+        //     }));
+        // }).catch(() => {
+        //     setIsSubmitted(false);
+        // })
 
-// triggerCreateAddress(accountAddressData).then(() => {
-//   setIsSubmitted(false);
-// }).catch(() => {
-//   setIsSubmitted(false);
-// });
+    if (!isFormValid()) {
+      triggerCreateAddress(accountAddressData)
+        .then((response) => {
+          console.log(response);
+          setIsSubmitted(false);
+        })
+        .catch(() => {
+          setIsSubmitted(false);
+        });
+    }
   }
   return (
     <MainLayout>
       <ProfileLayout>
         <Box>
-            <AddressManagment 
+          <AddressManagment
             handleSubmit={handleSubmit}
             isSubmitted={isSubmitted}
             accountAddressData={accountAddressData}
-            setAccountAddressData={ setAccountAddressData} />
-          </Box>
+            setAccountAddressData={setAccountAddressData}
+          />
+        </Box>
       </ProfileLayout>
     </MainLayout>
   );
 };
 
-
-
+export default Setting;
+function updateAddressData(addressDetailsData: addressDetailsData) {
+  throw new Error('Function not implemented.');
+}
