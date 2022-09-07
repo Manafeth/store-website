1c23ea04d35581c33f2ef151019aa7d3c0ad1c48
Nermen yehia <n.yehia@glt.co>
2022-09-07 14:53:38 +0200

    handle order summary integration

--- components/OrderSummary/index.tsx
+++ components/OrderSummary/index.tsx
@@ -1,81 +1,61 @@
-import React, { FC } from "react";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import { useTranslation } from "react-i18next";
-import { useCartModal } from "../../contexts/CartContext";
+import React, { FC } from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { useTranslation } from 'react-i18next';
+import { useCartModal } from '../../contexts/CartContext';
 
 const OrderSummary = () => {
   const [t] = useTranslation();
   const { cartData } = useCartModal();
-  // const calcPrice = (item) => {
-  //   return item.reduce((accumulator, object) => {
-  //     return accumulator + object.total;
-  //   }, 0);
-  // };
-
+  const result = cartData.reduce((total, currentValue) => total = total + currentValue.total,0);
+  const subTotal = cartData.reduce((total, currentValue) => total = total + currentValue.subTotal,0);
+  const checkoutAttributsTotal = cartData.reduce((total, currentValue) => total = total + currentValue.checkoutAttributsTotal,0);
+  console.log('result',result)
 
   return (
     <Box>
-      {cartData?.map((item) => {
-        return (
-          <>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                mb: 3,
-              }}
-            >
-              <Typography variant="h5" component="h1">
-                {t("cart.subTotal")}
-              </Typography>
-              <Typography
-                variant="h5"
-                component="h1"
-                sx={{ fontWeight: "700" }}
-              >
-                {t("common.sar")} 4,557.32
-              </Typography>
-            </Box>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                mb: 3,
-              }}
-            >
-              <Typography variant="h5" component="h1">
-                {t("common.estimatedDelivery")}
-              </Typography>
-              <Typography
-                variant="h5"
-                component="h1"
-                sx={{ fontWeight: "700" }}
-              >
-                {t("common.sar")} 10
-              </Typography>
-            </Box>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                mb: 3,
-              }}
-            >
-              <Typography variant="h5" component="h1">
-                {t("cart.total")}
-              </Typography>
-              <Typography
-                variant="h5"
-                component="h1"
-                sx={{ fontWeight: "700" }}
-              >
-                {t("common.sar")} 4,567.32 
-              </Typography>
-            </Box>
-          </>
-        );
-      })}
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          mb: 3,
+        }}
+      >
+        <Typography variant='h5' component='h1'>
+          {t('cart.subTotal')}
+        </Typography>
+        <Typography variant='h5' component='h1' sx={{ fontWeight: '700' }}>
+          {t('common.sar')} {subTotal}
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          mb: 3,
+        }}
+      >
+        <Typography variant='h5' component='h1'>
+        {t('checkOut.checkoutAttributsTotal')}
+        </Typography>
+        <Typography variant='h5' component='h1' sx={{ fontWeight: '700' }}>
+          {t('common.sar')} {checkoutAttributsTotal}
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          mb: 3,
+        }}
+      >
+        <Typography variant='h5' component='h1'>
+          {t('cart.total')}
+        </Typography>
+        <Typography variant='h5' component='h1' sx={{ fontWeight: '700' }}>
+          {t('common.sar')} {result}
+        </Typography>
+      </Box>
     </Box>
   );
 };
