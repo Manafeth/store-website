3c5186dcd35c17c7dba2a66f4b73b674b5070b13
Nermen yehia <n.yehia@glt.co>
2022-10-30 13:52:14 +0200

    create new category ui

--- components/FeaturedCategoriesSection/index.tsx
+++ components/FeaturedCategoriesSection/index.tsx
@@ -6,59 +6,43 @@ import FeaturedCategoryCard from '../FeaturedCategoryCard';
 import { CategoryData } from '../../types/categories';
 
 interface Props {
-    categories: CategoryData[]
+  categories: CategoryData[];
 }
 
 const FeaturedCategoriesSection: FC<Props> = ({ categories }) => {
- const category1 = categories[0];
- const category2 = categories[1];
- const category3 = categories[2];
- const category4 = categories[3];
+  const category1 = categories[0];
+  const category2 = categories[1];
+  const category3 = categories[2];
+  const category4 = categories[3];
   return (
     <Box component='section'>
-      <Container maxWidth={false} sx={{ px: {xs: 2, lg: 7.5} }}>
-            <Grid container spacing={1.25}>
-                {category1 && (
-                    <Grid item  xs={12} md={4}>
-                        <FeaturedCategoryCard isVertical data={category1} />
-                    </Grid>
-                )}
-                {categories.length > 3 ? (
-                    <Grid item  xs={12} md={4}>
-                        {category2 && (
-                            <Box mb={1.25}>
-                                <FeaturedCategoryCard data={category2} />
-                            </Box>
-                        )}
-                        {category3 && (
-                            <Box>
-                                <FeaturedCategoryCard data={category3} />
-                            </Box>
-                        )}
-                    </Grid>
-                ) : (
-                    <>
-                        {category2 && (
-                            <Grid item  xs={12} md={4}>
-                                <FeaturedCategoryCard isVertical data={category2} />
-                            </Grid>
-                        )}
-                        {category3 && (
-                            <Grid item  xs={12} md={4}>
-                                <FeaturedCategoryCard isVertical data={category3} />
-                            </Grid>
-                        )}
-                    </>
-                )}
-                {category4 && (
-                    <Grid item xs={12} md={4}>
-                        <FeaturedCategoryCard isVertical data={category4} />
-                    </Grid>
-                )}
+      <Container maxWidth={false} sx={{ px: { xs: 2, lg: 7.5 } }}>
+        <Grid container spacing={2} rowSpacing={4}>
+          {category1 && (
+            <Grid item xs={12} md={4}>
+              <FeaturedCategoryCard data={category1} />
             </Grid>
-        </Container>
+          )}
+          <Grid item xs={12} md={4}>
+            {category2 && <FeaturedCategoryCard data={category2} />}
+          </Grid>
+          <Grid item xs={12} md={4}>
+            {category3 && (
+              <Box>
+                <FeaturedCategoryCard data={category3} />
+              </Box>
+            )}
+          </Grid>
+
+          {category4 && (
+            <Grid item xs={12} md={4}>
+              <FeaturedCategoryCard isVertical data={category4} />
+            </Grid>
+          )}
+        </Grid>
+      </Container>
     </Box>
-  )
-}
+  );
+};
 
 export default FeaturedCategoriesSection;
