26e10e8aa67c8b2faf24edcb7cad6a8c40eadf2b
Akram Badah <akram.badah@gmail.com>
2022-11-03 04:02:24 +0200

    Updated the ui of category card

---
+++ components/CategoryEmptyState/index.tsx
@@ -0,0 +1,74 @@
+import React, { FC } from 'react'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+
+import Link from 'next/link';
+
+import { CategoryData } from '../../types/categories';
+import paths from '../../constants/paths';
+import useTranslation from 'next-translate/useTranslation';
+
+interface Props {
+    data: CategoryData
+}
+
+const CategoryCard: FC<Props> = ({ data }) => {
+    const {t} = useTranslation('common');
+  return (
+    <Link href={paths.categoryDetails(data.id)}>
+        <Box
+            sx={{
+            position: 'relative',
+            height: 300,
+            cursor: 'pointer',
+            }}
+        >
+            <Avatar src={data.imageFilePath?.orignialUrl || ''} alt='category' sx={{ width: '100%', height: '100%', borderRadius: 0 }}>
+                C
+            </Avatar>
+            <Box
+            sx={{
+                position: 'absolute',
+                bottom: '29px',
+                left: '50%',
+                transform: 'translateX(-50%)',
+                width: '80%',
+                height: '48px',
+                p: 4,
+                backgroundColor: 'rgba(255,255,255,0.5)',
+                boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.12)',
+                borderRadius: '5px',
+                textAlign: 'center',
+            }}
+            >
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '7px',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <Typography
+                    variant='h4'
+                    sx={{ fontWeight: '500', color: 'primary.dark' }}
+                >
+                    {data.name}
+                </Typography>
+                <Typography
+                    variant='h5'
+                    component='span'
+                    sx={{ color: 'regba(0,0,0,0,0.5)' }}
+                >
+                    {data.itemsCount} {t('items')}
+                </Typography>
+            </Box>
+            </Box>
+        </Box>
+    </Link>
+  )
+}
+
+export default CategoryCard;
--- components/FeaturedCategoriesSection/index.tsx
+++ components/FeaturedCategoriesSection/index.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import FeaturedCategoryCard from '../FeaturedCategoryCard';
+import CategoryCard from '../CategoryCard';
 import { CategoryData } from '../../types/categories';
 
 interface Props {
@@ -10,35 +10,16 @@ interface Props {
 }
 
 const FeaturedCategoriesSection: FC<Props> = ({ categories }) => {
-  const category1 = categories[0];
-  const category2 = categories[1];
-  const category3 = categories[2];
-  const category4 = categories[3];
+const categoriesList = categories.filter((item, index) => index <= 2)
   return (
     <Box component='section'>
       <Container maxWidth={false} sx={{ px: { xs: 2, lg: 7.5 } }}>
         <Grid container spacing={2} rowSpacing={4}>
-          {category1 && (
-            <Grid item xs={12} md={4}>
-              <FeaturedCategoryCard data={category1} />
+          {categoriesList.map((item) => (
+            <Grid item xs={12} md={4} key={item.id}>
+              <CategoryCard data={item} height={400} />
             </Grid>
-          )}
-          <Grid item xs={12} md={4}>
-            {category2 && <FeaturedCategoryCard data={category2} />}
-          </Grid>
-          <Grid item xs={12} md={4}>
-            {category3 && (
-              <Box>
-                <FeaturedCategoryCard data={category3} />
-              </Box>
-            )}
-          </Grid>
-
-          {category4 && (
-            <Grid item xs={12} md={4}>
-              <FeaturedCategoryCard isVertical data={category4} />
-            </Grid>
-          )}
+          ))}
         </Grid>
       </Container>
     </Box>
