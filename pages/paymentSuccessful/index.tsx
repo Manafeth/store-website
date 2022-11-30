bd1fba7b94bb0f4ed61c19ca267fc3a1000650d5
Akram Badah <akram.badah@gmail.com>
2022-11-30 14:45:32 +0200

    Fixed the style of successful payment page

--- pages/paymentSuccessful/index.tsx
+++ pages/paymentSuccessful/index.tsx
@@ -3,7 +3,7 @@ import Typography from '@mui/material/Typography';
 import useTranslation from 'next-translate/useTranslation';
 import Container from '@mui/material/Container';
 import Image from 'next/image';
-import paymentSuccessful from '../../assets/images/payment-successful.svg';
+import paymentSuccessful from '../../assets/images/successful-payment.png';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
@@ -35,15 +35,15 @@ const PaymentSuccessful = () => {
 
 
   return (
-    <Box component='main' pt={11.375} sx={{ backgroundColor: storeInfo.backgroundColor }}>
+    <Box component='main' pt={11.375} sx={{ backgroundColor: storeInfo.backgroundColor}}>
       <Header />
-      <Container sx={{ mt: 5 }}>
-        <Grid container spacing={{ xs: 2, lg: 5 }} rowSpacing={4}>
-          <Grid item xs={12} md={8}>
+      <Container>
+        <Grid container spacing={{ xs: 2, lg: 5 }} rowSpacing={4} alignItems={{ md: 'center' }} height='100vh' pt={{ xs: 10, md: 0 }}>
+          <Grid item xs={12} md={6} textAlign={{ xs: 'center', md: 'left' }}>
             <Typography
               variant='h1'
               component='h1'
-              sx={{ mb: 4, mt: 5, fontSize: { xs: '28px', md: '40px' } }}
+              sx={{ mb: 4.75, fontSize: { xs: '28px', md: '32px' } }}
             >
               {t('paymentSuccessful')}
             </Typography>
@@ -51,60 +51,56 @@ const PaymentSuccessful = () => {
               variant='h5'
               component='h1'
               sx={{
-                mb: 4, fontSize: { xs: '14px' }, lineHeight: '24px',
-                letterSpacing: '0.2px', width: '80%'
+                lineHeight: '24px',
+                letterSpacing: '0.2px',
+                maxWidth: '510px',
+                margin: { xs: '0 auto', md: '0' }
               }}
             >
               {t('paymentDescription')}
             </Typography>
             <Box
-        sx={{
-          display: 'flex',
-          justifyContent: { xs: 'space-between', sm: 'flex-start' },
-          pt: 7,
-          pb: 5,
-        }}
-      >
-            <Button
-              variant='contained'
-              color='secondary'
               sx={{
-                color: storeInfo.buttonTitelColor,
-                width: '219px',
-                height: '44px',
-                backgroundColor: storeInfo.buttonColor,
-                mr: '20px',
-                textTransform: 'lowercase'
+                display: 'flex',
+                justifyContent: { xs: 'center', md: 'flex-start' },
+                pt: 4.75,
               }}
-              onClick={handleRedirectTabbySuccessfullPayment}
             >
-              {t('viewOrders')}
-            </Button>
-            <Link href='/'>
-        <Button
-          variant='contained'
-          color='secondary'
-          sx={{
-            color: storeInfo.buttonTitelColor,
-            width: '219px',
-            height: '44px',
-            backgroundColor: ' linear-gradient(200.08deg, rgba(255, 255, 255, 0.0001) -10.81%, #E6EAEE 98.03%);',
-            mr: '20px',
-            textTransform: 'lowercase',
-            border: '0.4px solid #1995AD',
-            borderRadius: '8px'
-          }}
-        >
-          {t('continueShopping')}
-        </Button>
-        </Link>
+              <Button
+                variant='contained'
+                color='primary'
+                sx={{
+                  width: '219px',
+                  height: '44px',
+                  mr: '20px',
+                  textTransform: 'lowercase'
+                }}
+                onClick={handleRedirectTabbySuccessfullPayment}
+              >
+                {t('viewOrders')}
+              </Button>
+              <Link href='/'>
+                <Button
+                  variant='outlined'
+                  color='primary'
+                  sx={{
+                    width: '219px',
+                    height: '44px',
+                    mr: '20px',
+                    textTransform: 'lowercase',
+                    borderRadius: '8px'
+                  }}
+                >
+                  {t('continueShopping')}
+                </Button>
+              </Link>
             </Box>
           </Grid>
-          <Grid item xs={12} md={4}>
+          <Grid item xs={12} md={6} textAlign={{xs: 'center', md: 'right'}}>
             <Image
               src={paymentSuccessful}
-              width='410'
-              height='340'
+              width='400'
+              height='476'
               alt='Auth'
               style={{ maxWidth: '100%', height: 'auto' }}
             />
